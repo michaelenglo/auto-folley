a25c24223f48a503a84db6ed66428b02b034214f
Michael Englo <michaelenglo10@yahoo.co.id>
2017-06-24 14:31:45 -0700

    change .gitignore.txt to .gitignore

--- find_subtitle.py
+++ find_subtitle.py
@@ -14,4 +14,5 @@ def find_subtitle_in_srt(path_to_srt_file, subtitle_slice):
 	print subs[i].start.seconds
 	print subs[i].end.seconds
 
+
 find_subtitle_in_srt ('subtitles/taxi_driver.srt', "I once had a horse")
